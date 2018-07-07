2cef6758ba77fa73d9e10fe565a44e0bd866bcdb
Ricardo Reyes <ricardoricardoreyes@gmail.com>
2018-07-07 17:08:32 -0600

    Adding MapBox API key to restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoicmlja3ktcm9zZSIsImEiOiJjaml1dWd1OWoxMDJvM3BsZ2owcnZxc3d2In0.9silGpoF2MwwOH1qcgWkSw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
