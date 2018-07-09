ac9e6a6c443bdf82668b74e9251e02eeee7908fb
Ricardo Reyes <ricardoricardoreyes@gmail.com>
2018-07-08 19:22:51 -0600

    Edited sw.js in order to cache important files.

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
 //Main Service Worker file
+var cacheName = "res-reviews-001"
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/restaurant.html",
+        "/css/styles.css",
+        "/js/dbhelper.js",
+        "/js/main.js",
+        "/js/register.js",
+        "/js/restaurant_info.js"
+      ]);
+    })
+  );
+});
+
+
