9e90d0b070f6e8ba87641d801ac53c87436358c3
Ricardo Reyes <ricardoricardoreyes@gmail.com>
2018-07-09 20:24:35 -0600

    Added more resources to cache to the Service Worker.

--- sw.js
+++ sw.js
@@ -12,7 +12,19 @@ self.addEventListener("install", function(event) {
         "/js/dbhelper.js",
         "/js/main.js",
         "/js/register.js",
-        "/js/restaurant_info.js"
+        "/js/restaurant_info.js",
+        "/data/restaurants.json",
+        "/sw.js",
+        "/restaurant.html?id=1",
+        "/restaurant.html?id=2",
+        "/restaurant.html?id=3",
+        "/restaurant.html?id=4",
+        "/restaurant.html?id=5",
+        "/restaurant.html?id=6",
+        "/restaurant.html?id=7",
+        "/restaurant.html?id=8",
+        "/restaurant.html?id=9",
+        "/restaurant.html?id=10"
       ]);
     })
   );
